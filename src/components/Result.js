081f5e66c51d99dcb8c608f6adb2556c71b9b9b3
shuwenju <jusendia@gmail.com>
2023-05-22 19:17:32 -0400

    added css on goals and result

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,71 +1,91 @@
-import React from "react";
+import React from 'react'
 
 const Result = ({ userData, handleTdeeCalculation, setShowTDEE, showTDEE }) => {
+  const { age, gender, activityLevel, height, weight } = userData
   const calculateTDEE = () => {
-    let activityMultiplier;
-    switch (userData.activityLevel) {
-      case "sedentary":
-        activityMultiplier = 1.2;
-        break;
-      case "lightly-active":
-        activityMultiplier = 1.375;
-        break;
-      case "moderately-active":
-        activityMultiplier = 1.55;
-        break;
-      case "very-active":
-        activityMultiplier = 1.725;
-        break;
-      case "super-active":
-        activityMultiplier = 1.9;
-        break;
-      default:
-        activityMultiplier = 1.2;
-        break;
+    if (
+      age === '' ||
+      gender === '' ||
+      activityLevel === '' ||
+      height === '' ||
+      weight === ''
+    ) {
+      return 0
     }
-    const tdee =
-      (10 * userData.weight +
-        6.25 * userData.height -
-        5 * userData.age +
-        (userData.gender === "female" ? -161 : 5)) *
-      activityMultiplier;
 
-    return Math.round(tdee);
-  };
+    let activityMultiplier
+
+    switch (activityLevel) {
+      case 'sedentary':
+        activityMultiplier = 1.2
+        break
+      case 'lightly-active':
+        activityMultiplier = 1.375
+        break
+      case 'moderately-active':
+        activityMultiplier = 1.55
+        break
+      case 'very-active':
+        activityMultiplier = 1.725
+        break
+      case 'super-active':
+        activityMultiplier = 1.9
+        break
+      default:
+        activityMultiplier = 1.2
+        break
+    }
+
+    const tdee =
+      (10 * weight +
+        6.25 * height -
+        5 * age +
+        (gender === 'female' ? -161 : 5)) *
+      activityMultiplier
+
+    return Math.round(tdee)
+  }
 
   const handleShowTDEE = () => {
-    setShowTDEE(true); // Set showTDEE state to true when the button is clicked
-    handleTdeeCalculation(tdee);
-  };
+    setShowTDEE(true) // Set showTDEE state to true when the button is clicked
+    handleTdeeCalculation(tdee)
+    console.log(userData)
+  }
 
-  const tdee = calculateTDEE(); // Calculate TDEE
+  const tdee = calculateTDEE() // Calculate TDEE
 
   return (
-    <div>
-      <button
-        className="bg-blue-500 text-white px-4 py-2 rounded-md"
-        onClick={handleShowTDEE}
-      >
-        Calculate TDEE
-      </button>
-      {showTDEE && !isNaN(tdee) ? (
-        <>
-          <h2>
-            You are {userData.age} y/o {userData.gender} who is{" "}
-            {userData.height} cm and {userData.weight} kg with{" "}
-            {userData.activityLevel} lifestyle.
-          </h2>
-          <h2>
-            Based on your data, your best estimate for maintenance calories is{" "}
-            {tdee} per day.
-            <h2></h2>
-          </h2>
-        </>
-      ) : showTDEE ? (
-        <>Please input your info</>
-      ) : null}
+    <div className='goal-page flex justify-center items-center'>
+      <div className='p-6 rounded-lg mx-auto max-w-screen-lg'>
+        <div className='flex justify-center'>
+          <button
+            className='bg-blue-500 text-white px-4 py-2 rounded-md w-[200px]'
+            onClick={handleShowTDEE}
+          >
+            Calculate TDEE
+          </button>
+        </div>
+        {showTDEE && tdee != 0 ? (
+          <div className='mt-6'>
+            <h2 className='text-gray-500'>
+              You are {userData.age} y/o {userData.gender} who is{' '}
+              {userData.height} cm and {userData.weight} kg with{' '}
+              {userData.activityLevel} lifestyle.
+            </h2>
+            <h2 className='text-gray-500'>
+              Based on your data, your best estimate for maintenance calories is{' '}
+              {tdee} per day.
+              <h2></h2>
+            </h2>
+          </div>
+        ) : showTDEE ? (
+          <div className='mt-6'>
+            <h2 className='text-red-500'>Please input your info first</h2>
+          </div>
+        ) : null}
+      </div>
     </div>
-  );
-};
+  )
+}
 
-export default Result;
+export default Result
--- src/components/UserGoals.js
+++ src/components/UserGoals.js
@@ -1,5 +1,5 @@
-import React from "react";
-import Result from "./Result";
+import React from 'react'
+import Result from './Result'
 
 const UserGoals = ({
   handleUserDataChange,
@@ -12,90 +12,92 @@ const UserGoals = ({
   setShowTDEE,
 }) => {
   const handleGoalChange = (event) => {
-    const goal = event.currentTarget.value;
-    setSelectedGoal(goal); // Update selectedGoal state
-    handleUserDataChange(userInfo, goal); // Pass the selected goal to the parent component
-  };
+    const goal = event.currentTarget.value
+    setSelectedGoal(goal) // Update selectedGoal state
+    handleUserDataChange(userInfo, goal) // Pass the selected goal to the parent component
+  }
 
   return (
-    <div className="goal-page flex justify-center items-center h-[calc(100vh-4rem)]">
-      <div className="bg-gray-200 p-6 rounded-lg">
-        <h3 className="text-lg font-semibold mb-4">
-          Select your fitness goal:
-        </h3>
-        <div className="flex flex-row space-x-4">
-          <label
-            className={`flex items-center ${
-              selectedGoal === "weight-loss"
-                ? "bg-blue-500 text-white animate-bounce"
-                : "bg-blue-200 text-blue-500"
-            } px-4 py-2 rounded cursor-pointer transition-colors`}
-          >
-            <input
-              type="radio"
-              name="goal"
-              value="weight-loss"
-              checked={selectedGoal === "weight-loss"}
-              className="hidden"
-              onClick={handleGoalChange}
-              readOnly
-            />
-            Weight Loss
-          </label>
-          <label
-            className={`flex items-center ${
-              selectedGoal === "muscle-gain"
-                ? "bg-green-500 text-white animate-bounce"
-                : "bg-green-200 text-green-500"
-            } px-4 py-2 rounded cursor-pointer transition-colors`}
-          >
-            <input
-              type="radio"
-              name="goal"
-              value="muscle-gain"
-              checked={selectedGoal === "muscle-gain"}
-              className="hidden"
-              onClick={handleGoalChange}
-              readOnly
-            />
-            Muscle Gain
-          </label>
-          <label
-            className={`flex items-center ${
-              selectedGoal === "flexibility"
-                ? "bg-yellow-500 text-white animate-bounce"
-                : "bg-yellow-200 text-yellow-500"
-            } px-4 py-2 rounded cursor-pointer transition-colors`}
-          >
-            <input
-              type="radio"
-              name="goal"
-              value="flexibility"
-              checked={selectedGoal === "flexibility"}
-              className="hidden"
-              onClick={handleGoalChange}
-              readOnly
-            />
-            Flexibility
-          </label>
-          <label
-            className={`flex items-center ${
-              selectedGoal === "cardiovascular"
-                ? "bg-red-500 text-white animate-bounce"
-                : "bg-red-200 text-red-500"
-            } px-4 py-2 rounded cursor-pointer transition-colors`}
-          >
-            <input
-              type="radio"
-              name="goal"
-              value="cardiovascular"
-              checked={selectedGoal === "cardiovascular"}
-              className="hidden"
-              onClick={handleGoalChange}
-              readOnly
-            />
-            Cardiovascular
-          </label>
+    <div>
+      <div className='goal-page flex justify-center items-center h-[calc(50vh-2rem)]'>
+        <div className='bg-gray-200 p-6 rounded-lg mx-auto max-w-screen-lg'>
+          <h3 className='text-lg font-semibold mb-4'>
+            Select your fitness goal:
+          </h3>
+          <div className='flex flex-row space-x-4'>
+            <label
+              className={`flex items-center ${
+                selectedGoal === 'weight-loss'
+                  ? 'bg-blue-500 text-white animate-bounce'
+                  : 'bg-blue-200 text-blue-500'
+              } px-4 py-2 rounded cursor-pointer transition-colors`}
+            >
+              <input
+                type='radio'
+                name='goal'
+                value='weight-loss'
+                checked={selectedGoal === 'weight-loss'}
+                className='hidden'
+                onClick={handleGoalChange}
+                readOnly
+              />
+              Weight Loss
+            </label>
+            <label
+              className={`flex items-center ${
+                selectedGoal === 'muscle-gain'
+                  ? 'bg-green-500 text-white animate-bounce'
+                  : 'bg-green-200 text-green-500'
+              } px-4 py-2 rounded cursor-pointer transition-colors`}
+            >
+              <input
+                type='radio'
+                name='goal'
+                value='muscle-gain'
+                checked={selectedGoal === 'muscle-gain'}
+                className='hidden'
+                onClick={handleGoalChange}
+                readOnly
+              />
+              Muscle Gain
+            </label>
+            <label
+              className={`flex items-center ${
+                selectedGoal === 'flexibility'
+                  ? 'bg-yellow-500 text-white animate-bounce'
+                  : 'bg-yellow-200 text-yellow-500'
+              } px-4 py-2 rounded cursor-pointer transition-colors`}
+            >
+              <input
+                type='radio'
+                name='goal'
+                value='flexibility'
+                checked={selectedGoal === 'flexibility'}
+                className='hidden'
+                onClick={handleGoalChange}
+                readOnly
+              />
+              Flexibility
+            </label>
+            <label
+              className={`flex items-center ${
+                selectedGoal === 'cardiovascular'
+                  ? 'bg-red-500 text-white animate-bounce'
+                  : 'bg-red-200 text-red-500'
+              } px-4 py-2 rounded cursor-pointer transition-colors`}
+            >
+              <input
+                type='radio'
+                name='goal'
+                value='cardiovascular'
+                checked={selectedGoal === 'cardiovascular'}
+                className='hidden'
+                onClick={handleGoalChange}
+                readOnly
+              />
+              Cardiovascular
+            </label>
+          </div>
         </div>
       </div>
       <Result
@@ -105,7 +107,7 @@ const UserGoals = ({
         setShowTDEE={setShowTDEE}
       />
     </div>
-  );
-};
+  )
+}
 
-export default UserGoals;
+export default UserGoals
