853a53093e35ad0c00a39b56b5c611984fd8abb3
Bert Gordon <bertg1427@gmail.com>
2023-05-23 17:20:51 -0400

    revert changes

--- src/App.js
+++ src/App.js
@@ -1,61 +1,62 @@
-import UserGoals from './components/UserGoals'
-import UserInfo from './components/UserInfo'
-import Nutrition from './components/Nutrition'
-import Navbar from './components/Navbar'
-import Workout from './components/Workout'
-import { Routes, Route } from 'react-router-dom'
-import { useState, useEffect } from 'react'
+import UserGoals from "./components/UserGoals";
+import UserInfo from "./components/UserInfo";
+import Nutrition from "./components/Nutrition";
+import Navbar from "./components/Navbar";
+import Workout from "./components/Workout";
+import { Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 function App() {
   const [userData, setUserData] = useState({
-    goal: '',
-    age: '',
-    gender: '',
-    height: '',
-    weight: '',
-    activityLevel: '',
+    goal: "",
+    age: "",
+    gender: "",
+    height: "",
+    weight: "",
+    activityLevel: "",
     workoutFreq: 0,
-  })
-  const [tdee, setTdee] = useState(0)
-  const usertdee = tdee === 0 ? 2000 : tdee
+  });
+  const [tdee, setTdee] = useState(0);
+  const usertdee = tdee === 0 ? 2000 : tdee;
   const [userInfo, setUserInfo] = useState({
-    age: '',
-    gender: 'male',
-    height: '',
-    weight: '',
-    activityLevel: 'sedentary',
-  })
-  const [selectedGoal, setSelectedGoal] = useState('')
-  const [workoutFreq, setWorkoutFreq] = useState(0)
-  const [showTDEE, setShowTDEE] = useState(false)
-  const [foodLog, setFoodLog] = useState([])
-  const [fat, setFat] = useState(0)
-  const [carbs, setCarbs] = useState(0)
-  const [protein, setProtein] = useState(0)
-  const [caloriesLeft, setCaloriesLeft] = useState(usertdee)
+    age: "",
+    gender: "male",
+    height: "",
+    weight: "",
+    activityLevel: "sedentary",
+  });
+  const [selectedGoal, setSelectedGoal] = useState("");
+  const [workoutFreq, setWorkoutFreq] = useState(0);
+
+  const [showTDEE, setShowTDEE] = useState(false);
+  const [foodLog, setFoodLog] = useState([]);
+  const [fat, setFat] = useState(0);
+  const [carbs, setCarbs] = useState(0);
+  const [protein, setProtein] = useState(0);
+  const [caloriesLeft, setCaloriesLeft] = useState(usertdee);
   const handleTdeeCalculation = (tdeeValue) => {
-    setTdee(tdeeValue)
-  }
+    setTdee(tdeeValue);
+  };
 
   const handleUserDataChange = (userInfoData, userGoalData, userFreqData) => {
-    const updatedUserData = { ...userInfoData, userGoalData, userFreqData }
-    console.log(updatedUserData)
-    setUserData(updatedUserData)
-  }
-
+    const updatedUserData = { ...userInfoData, userGoalData, userFreqData };
+    console.log(updatedUserData);
+    setUserData(updatedUserData);
+  };
   useEffect(() => {
-    setCaloriesLeft(usertdee)
+    setCaloriesLeft(usertdee);
     // console.log(
     //   'tdee ' + tdee,
     //   ' usertdee ' + usertdee + ' caloriesleft: ' + caloriesLeft
     // )
-  }, [usertdee])
+  }, [usertdee]);
+
   return (
     <div>
       <Navbar />
       <Routes>
         <Route
-          path='/'
+          path="/"
           element={
             <UserInfo
               handleUserDataChange={handleUserDataChange}
@@ -69,7 +70,7 @@ function App() {
           }
         />
         <Route
-          path='goals'
+          path="goals"
           element={
             <UserGoals
               handleUserDataChange={handleUserDataChange}
@@ -86,7 +87,7 @@ function App() {
           }
         ></Route>
         <Route
-          path='calculator'
+          path="calculator"
           element={
             <Nutrition
               tdee={tdee}
@@ -106,12 +107,12 @@ function App() {
           }
         ></Route>
         <Route
-          path='workouts'
+          path="workouts"
           element={<Workout userData={userData} />}
         ></Route>
       </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
