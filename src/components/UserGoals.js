95e79590c0c7c46998076ca8d369d2c086d51cab
mrabi24 <120145506+mrabi24@users.noreply.github.com>
2023-05-24 15:17:58 -0400

    fixed preservation of selected frequency

--- src/components/UserGoals.js
+++ src/components/UserGoals.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import '../App.css'
 
@@ -24,6 +24,10 @@ const UserGoals = ({
     setSelectedFrequency(frequency)
     handleUserDataChange(userInfo, selectedGoal, frequency)
   }
+
+  useEffect(() => {
+    setSelectedFrequency(workoutFreq);
+  }, [workoutFreq]);
 
   return (
     <div>
